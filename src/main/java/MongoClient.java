ae61154163c0ae52c19ccd546575de87c9e3a486
Jagannath <jaganath1512@gmail.com>
2017-04-09 17:29:32 -0500

    base project setup

---
+++ src/main/java/MongoClient.java
@@ -0,0 +1,13 @@
+
+
+
+import com.mongodb.MongoClient;
+
+import java.util.Arrays;/**
+ * Created by Lennovo on 4/9/2017.
+ */
+public class MongoConnection {
+    MongoClient mongo = new MongoClient( "localhost" , 27017 );
+
+
+}
